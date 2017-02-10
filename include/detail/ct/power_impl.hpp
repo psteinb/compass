cabf1420dbd21b424496bc46b390fbd0c0b6490f
Peter Steinbach <steinbac@mpi-cbg.de>
2017-02-10 16:56:46 +0100

    added empty ct power header for now

---
+++ include/detail/ct/power_impl.hpp
@@ -0,0 +1,21 @@
+#ifndef COMPASS_CT_CT_POWER_IMPL_H_
+#define COMPASS_CT_CT_POWER_IMPL_H_
+
+
+#include <array>
+#include <bitset>
+#include <cstdint>
+
+namespace compass {
+
+
+  namespace compiletime {
+
+    //access to the PVR register is priviledge on power
+    //os there is nothing here yet
+
+
+  };
+
+};
+#endif /* COMPASS_CT_LLVM_IMPL_H_ */
