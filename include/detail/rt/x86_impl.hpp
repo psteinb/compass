4c7200466f7df0ce97a878ae9f86d71933767944
Peter Steinbach <steinbac@mpi-cbg.de>
2017-02-10 17:03:04 +0100

    removed obsolete comments

--- include/detail/rt/x86_impl.hpp
+++ include/detail/rt/x86_impl.hpp
@@ -1,5 +1,5 @@
-#ifndef _COMPASS_X86_IMPL_H_
-#define _COMPASS_X86_IMPL_H_
+#ifndef COMPASS_RT_X86_IMPL_H_
+#define COMPASS_RT_X86_IMPL_H_
 
 #include "../tags.hpp"
 
@@ -60,8 +60,6 @@ namespace compass {
           std::cerr << "unsupported cpuid level detected\n";
         }
 
-        // std::uint32_t bit25 = (1<<25);
-        // bool value = (regs[3] & bit25) > 0;
         bool value = regs[ct::edx].test(25);
         return value;
       }
@@ -74,8 +72,6 @@ namespace compass {
           std::cerr << "unsupported cpuid level detected\n";
         }
 
-        // std::uint32_t bit26 = (1<<26);
-        // bool value = (regs[3] & bit26) > 0;
         bool value = regs[ct::edx].test(26);
         return value;
       }
@@ -88,8 +84,6 @@ namespace compass {
           std::cerr << "unsupported cpuid level detected\n";
         }
 
-        // std::uint32_t check_bits = (1<<9) | 1 | (1<<3);
-        // bool value = (regs[2] & check_bits) > 0;
         bool value = regs[ct::ecx].test(9) | regs[ct::ecx].test(0) | regs[ct::ecx].test(3);
         return value;
       }
@@ -103,11 +97,7 @@ namespace compass {
           std::cerr << "unsupported cpuid level detected\n";
         }
 
-        // std::uint32_t check_bits = (1<<19) | (1<<20);
-        // bool value = (regs[2] & check_bits) > 0;
         bool value = regs[ct::ecx].test(19) | regs[ct::ecx].test(20);
-
-        //TODO: check sse4a if AMD is detected?
 
         return value;
       }
@@ -120,9 +110,6 @@ namespace compass {
           std::cerr << "unsupported cpuid level detected\n";
         }
 
-
-        // std::uint32_t check_bits = (1<<28);
-        // bool value = (regs[2] & check_bits) > 0;
         bool value = regs[ct::ecx].test(28);
 
         return value;
@@ -132,8 +119,6 @@ namespace compass {
 
         std::array<std::bitset<32>,4> regs = ct::cpuid(7);
 
-        // std::uint32_t check_bits = (1<<5);
-        // bool value = (regs[1] & check_bits) > 0;
         bool value = regs[ct::ebx].test(5);
 
         return value;
@@ -143,4 +128,4 @@ namespace compass {
   };
 
 };
-#endif /* _COMPASS_X86_IMPL_H_ */
+#endif /* COMPASS_RT_X86_IMPL_H_ */
