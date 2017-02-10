ab04d66d6d323e00f2048a8834c5ab39a7518fd6
Peter Steinbach <steinbac@mpi-cbg.de>
2017-02-10 17:02:51 +0100

    refactored ifndefn include decisions to incorporate power

--- include/detail/dispatch.hpp
+++ include/detail/dispatch.hpp
@@ -1,28 +1,44 @@
-#ifndef _COMPASS_DETAIL_H_
-#define _COMPASS_DETAIL_H_
+#ifndef COMPASS_DETAIL_H_
+#define COMPASS_DETAIL_H_
 
 
 #include "tags.hpp"
 
 //compiletime code
+#if defined(__linux__)
 
-#ifdef __clang__
-#include "ct/llvm_impl.hpp"
-#else
-#ifdef __GNUC__
-#include "ct/gnu_impl.hpp"
-#endif
-#endif
+    #if (defined(__powerpc64__) || defined(__powerpc__))
+        #include "ct/power_impl.hpp"
+    #else
+        #if defined(__clang__)
+            #include "ct/llvm_impl.hpp"
+        #else if defined(__GNUC__)
+                #include "ct/gnu_impl.hpp"
+            #endif
+        #endif
+    #endif
 
-#ifdef _MSC_BUILD
-#include "ct/msvc_impl.hpp"
+#else //not linux
+
+    #ifdef __APPLE__
+        #include "ct/llvm_impl.hpp"
+    #else //not __APPLE__
+        #ifdef _MSC_BUILD
+            #include "ct/msvc_impl.hpp"
+        #endif
+    #endif
+
 #endif
 
 #include "ct/preprocessor_impl.hpp"
 
 //runtime code
+#if defined(__x86_64) || defined(__x86_64__) || defined(__x86)
+    #include "rt/x86_impl.hpp"
+#endif
 
-#include "rt/x86_impl.hpp"
+#if (defined(__powerpc64__) || defined(__powerpc__))
+    #include "rt/power_impl.hpp"
+#else
 
-
-#endif /* _COMPASS_DETAIL_H_ */
+#endif /* COMPASS_DETAIL_H_ */
