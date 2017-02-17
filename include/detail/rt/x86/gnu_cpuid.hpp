e0798102645b9ac94e53152665a1035831098ab8
Peter Steinbach <steinbac@mpi-cbg.de>
2017-02-17 15:31:40 +0100

    added my own implementation of extended cpuid

--- include/detail/rt/x86/gnu_cpuid.hpp
+++ include/detail/rt/x86/gnu_cpuid.hpp
@@ -21,7 +21,22 @@ namespace compass {
 
   namespace runtime {
 
+    static int extended_get_cpuid(std::uint32_t* regs, std::uint32_t in_eax = 0,std::uint32_t in_ecx = 0){
 
+      unsigned int clean_level = in_eax & 0x80000000;
+
+      if (__get_cpuid_max (clean_level, 0) < in_eax)
+        return 0;
+
+      __cpuid_count(in_eax, in_ecx,
+                    regs[ct::eax],
+                    regs[ct::ebx],
+                    regs[ct::ecx],
+                    regs[ct::edx]
+        );
+
+      return 1;
+    }
 
     static std::array<std::uint32_t,4> cpuid_to_int(std::uint32_t in_eax = 0,
                                                     std::uint32_t in_ebx = 0,
@@ -30,12 +45,17 @@ namespace compass {
 
       static std::array<std::uint32_t,4> value = {in_eax,in_ebx,in_ecx,in_edx};
 
+#ifdef __get_cpuid_count
       int cpuid_rvalue = __get_cpuid_count(in_eax,
                                            in_ecx,
                                            &value[ct::eax],
                                            &value[ct::ebx],
                                            &value[ct::ecx],
                                            &value[ct::edx]);
+#else
+      int cpuid_rvalue = extended_get_cpuid(&value[0],in_eax,in_ecx);
+#endif
+
       if(cpuid_rvalue < 1){
         value = {0,0,0,0};
         return value;
