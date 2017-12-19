1fb5312fa598a5f6961b5f655fc4a6d306fff9ee
Peter Steinbach <steinbac@mpi-cbg.de>
2017-12-19 13:45:13 +0100

    ported to catch2

--- tests/test_bitview.cpp
+++ tests/test_bitview.cpp
@@ -1,105 +1,100 @@
-#define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE TEST_BITVIEW
-#include "boost/test/unit_test.hpp"
-
-#include <bitset>
-
+#include "catch.hpp"
 
 #include "detail/bit_view.hpp"
 
+#include <bitset>
 
-BOOST_AUTO_TEST_SUITE( basics )
+TEST_CASE( "basics" ){
 
-BOOST_AUTO_TEST_CASE( construction ){
+  SECTION( "construction" ){
 
-  int num = 7;
-  compass::utility::bit_view<int> view(num);
+    int num = 7;
+    compass::utility::bit_view<int> view(num);
 
-  BOOST_CHECK(view.value_ == 7);
+    REQUIRE(view.value_ == 7);
 
-}
+  }
 
-BOOST_AUTO_TEST_CASE( assignment ){
+  SECTION( "assignment" ){
 
-  int num = 7;
-  compass::utility::bit_view<int> view(num);
-  compass::utility::bit_view<int> temp = view;
+    int num = 7;
+    compass::utility::bit_view<int> view(num);
+    compass::utility::bit_view<int> temp = view;
 
-  BOOST_CHECK(temp.value_ == 7);
+    REQUIRE(temp.value_ == 7);
 
-}
+  }
 
-BOOST_AUTO_TEST_CASE( copy ){
+  SECTION( "copy" ){
 
-  int num = 7;
-  compass::utility::bit_view<int> view(num);
-  compass::utility::bit_view<int> temp(view);
+    int num = 7;
+    compass::utility::bit_view<int> view(num);
+    compass::utility::bit_view<int> temp(view);
 
-  BOOST_CHECK(temp.value_ == 7);
+    REQUIRE(temp.value_ == 7);
 
-}
-
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE( logical )
-
-BOOST_AUTO_TEST_CASE( single_test_within_bounds ){
-  int num = 42;
-
-  std::bitset<32> bs_num(num);
-  compass::utility::bit_view<int> view(num);
-
-
-  BOOST_CHECK(bs_num.any());
-  BOOST_CHECK_EQUAL(bs_num.test(1),view.test(1));
-}
-
-BOOST_AUTO_TEST_CASE( test_all_within_bounds ){
-
-  int num = 42;
-
-  std::bitset<32> bs_num(num);
-  compass::utility::bit_view<int> view(num);
-
-  for(int i = 0;i<32;++i)
-    BOOST_CHECK_EQUAL(bs_num.test(i),view.test(i));
-}
-
-BOOST_AUTO_TEST_CASE( test_outof_bounds ){
-
-  int num = 42;
-
-  compass::utility::bit_view<int> view(num);
-
-  BOOST_CHECK_NE(view.test(50),true);
+  }
 }
 
 
-BOOST_AUTO_TEST_SUITE_END()
+TEST_CASE( "logical" ){
 
-BOOST_AUTO_TEST_SUITE( mutation )
+  SECTION( "single_test_within_bounds" ){
+    int num = 42;
 
-BOOST_AUTO_TEST_CASE( set_lsb ){
+    std::bitset<32> bs_num(num);
+    compass::utility::bit_view<int> view(num);
+
+
+    REQUIRE(bs_num.any());
+    REQUIRE(bs_num.test(1) == view.test(1));
+  }
+
+  SECTION( "test_all_within_bounds" ){
+
+    int num = 42;
+
+    std::bitset<32> bs_num(num);
+    compass::utility::bit_view<int> view(num);
+
+    for(int i = 0;i<32;++i)
+      REQUIRE(bs_num.test(i) == view.test(i));
+  }
+
+  SECTION( "test_outof_bounds" ){
+
+    int num = 42;
+
+    compass::utility::bit_view<int> view(num);
+
+    REQUIRE(view.test(50)!=true);
+  }
+
+}
+
+TEST_CASE( "mutation" ){
+
+SECTION( "set_lsb" ){
 
   int num = 42;
 
   compass::utility::bit_view<int> view(num);
   view.set(0);
 
-  BOOST_CHECK_NE(num,42);
+  REQUIRE(num!=42);
 }
 
-BOOST_AUTO_TEST_CASE( set_msb ){
+SECTION( "set_msb" ){
 
   int num = 42;
 
   compass::utility::bit_view<int> view(num);
   view.set(31);
 
-  BOOST_CHECK_NE(num,42);
+  REQUIRE(num!=42);
 }
 
-BOOST_AUTO_TEST_CASE( set_lsb_correct ){
+SECTION( "set_lsb_correct" ){
 
   int num = 42;
 
@@ -110,12 +105,12 @@ BOOST_AUTO_TEST_CASE( set_lsb_correct ){
   compass::utility::bit_view<int> view(num);
   view.set(0);
 
-  BOOST_CHECK_NE(rnum,42);
-  BOOST_CHECK_NE(num,42);
-  BOOST_CHECK_EQUAL(num,rnum);
+  REQUIRE(rnum!=42);
+  REQUIRE(num !=42);
+  REQUIRE(num==rnum);
 }
 
-BOOST_AUTO_TEST_CASE( set_msb_correct ){
+SECTION( "set_msb_correct" ){
 
   int num = 42;
 
@@ -127,10 +122,9 @@ BOOST_AUTO_TEST_CASE( set_msb_correct ){
   view.set(31);
 
 
-  BOOST_CHECK_NE(num,42);
-  BOOST_CHECK_NE(rnum,42);
-  BOOST_CHECK_EQUAL(num,rnum);
+  REQUIRE(num !=42);
+  REQUIRE(rnum!=42);
+  REQUIRE(num==rnum);
 }
 
-
-BOOST_AUTO_TEST_SUITE_END()
+}
