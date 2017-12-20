dcdd5d63e5603f8be3f495aed3467532a5d97de5
Peter Steinbach <steinbac@mpi-cbg.de>
2017-12-20 15:07:28 +0100

    renamed test sections

--- tests/test_compass_sizes.cpp
+++ tests/test_compass_sizes.cpp
@@ -9,11 +9,9 @@
 #include <algorithm>
 
 
+TEST_CASE_METHOD( host_reference, "cacheline_size" ){
 
-
-TEST_CASE_METHOD( host_reference, "machine_specific" ){
-
-  SECTION( "L1d_cacheline_size" ){
+  SECTION( "L1d" ){
 
     auto value = compass::runtime::size::cacheline::level(1);
 
@@ -21,11 +19,50 @@ TEST_CASE_METHOD( host_reference, "machine_specific" ){
 
   }
 
+  SECTION( "L2" ){
+
+    auto value = compass::runtime::size::cacheline::level(2);
+
+    REQUIRE(value==expected_L2_linesize);
+
+  }
+
+  SECTION( "L3" ){
+
+    auto value = compass::runtime::size::cacheline::level(3);
+
+    REQUIRE(value==expected_L3_linesize);
+
+  }
+
+
+}
+
+
+TEST_CASE_METHOD( host_reference, "cache_size" ){
+
   SECTION( "L1d_cache_size" ){
 
     auto value = compass::runtime::size::cache::level(1);
 
-    REQUIRE(value==expected_L1d_size_kB);
+    REQUIRE(value==expected_L1d_size_kB*1024);
 
   }
+
+  SECTION( "L2_cache_size" ){
+
+    auto value = compass::runtime::size::cache::level(2);
+
+    REQUIRE(value==expected_L2_size_kB*1024);
+
+  }
+
+  SECTION( "L3_cache_size" ){
+
+    auto value = compass::runtime::size::cache::level(3);
+
+    REQUIRE(value==expected_L3_size_kB*1024);
+
+  }
+
 }
