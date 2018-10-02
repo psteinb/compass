16d264e406de6b065e3a1e543924ddf4dbd90945
Peter Steinbach <steinbach@scionics.de>
2018-10-02 10:55:29 +0200

    comparing the device name is optional

--- tests/test_build_machine.cpp
+++ tests/test_build_machine.cpp
@@ -39,7 +39,9 @@ TEST_CASE_METHOD( host_reference, "machine_specific" ){
 
     auto value = compass::runtime::device_name();
 
-    REQUIRE(value.empty()!=true);
+    if(expected_device_name.size() > 0){
+        REQUIRE(value.empty()!=true);}
+
     REQUIRE_THAT(value, Catch::Matchers::Contains(expected_device_name) );
 
   }
